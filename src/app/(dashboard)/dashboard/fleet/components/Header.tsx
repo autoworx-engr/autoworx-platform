07a7f0626109c8a8747da2306981e40082e9ccff
Mh-Monzil <monzil246@gmail.com>
2025-09-30 16:08:55 +0600

    refactor: update placeholder text in Header component for improved clarity

--- src/app/(dashboard)/dashboard/fleet/components/Header.tsx
+++ src/app/(dashboard)/dashboard/fleet/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
             name="search"
             type="text"
             className="w-full rounded-md border border-white px-4 py-1 focus:outline-none"
-            placeholder="Search"
+            placeholder="Search by name, email, phone..."
             onChange={(e) => setFilter({ search: e.target.value })}
           />
         </div>
