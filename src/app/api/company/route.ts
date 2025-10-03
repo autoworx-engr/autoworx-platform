96a198820885335bf4d5190e127654141acd6af9
rayhanmujumdar <rayhanmujumdar0177@gmail.com>
2025-10-04 01:15:28 +0600

    chore: update dependencies and improve error handling
    - Added "@vercel/speed-insights" to package.json for performance monitoring.
    - Removed unused imports from CannedLabor.tsx and InventoryTableRow.tsx.
    - Enhanced error handling in the GET request for fetching companies in route.ts.
    - Integrated SpeedInsights component in layout.tsx for performance insights.
    - Updated react-qr-reader dependency to a forked version in package.json.

--- src/app/api/company/route.ts
+++ src/app/api/company/route.ts
@@ -2,7 +2,14 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, res: Response) {
-  const allCompanies = await db.company.findMany({});
-
-  return NextResponse.json(allCompanies);
+  try {
+    const allCompanies = await db.company.findMany({});
+    return NextResponse.json(allCompanies);
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    return NextResponse.json(
+      { error: "All company fetch failed" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import TopLoader from "../components/TopLoader";
 import "./globals.css";
-import { redirect } from "next/navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -76,6 +76,7 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <PublicEnvScript />
+        <SpeedInsights />
         {/* <link
           rel="apple-touch-icon"
           sizes="512x512"
