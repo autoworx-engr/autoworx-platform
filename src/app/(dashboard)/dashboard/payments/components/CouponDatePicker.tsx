f0ac1fd20840138738b7e7349760a055ddb2cd38
unknown <Ashik-Rahman100>
2025-10-13 01:02:36 +0600

    issue/fix: fixed TextFieldProps type issue

--- src/app/(dashboard)/dashboard/payments/components/CouponDatePicker.tsx
+++ src/app/(dashboard)/dashboard/payments/components/CouponDatePicker.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { TextFieldProps } from "@mui/material";
 import Box from "@mui/material/Box";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
