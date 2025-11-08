626aabc4bea7a7ad470424d4657c5c7f85d6df85
rayhanmujumdar <rayhanmujumdar0177@gmail.com>
2025-11-09 02:30:55 +0600

    Refactor useClientQuery to use a constant for the query key, improving code readability and maintainability.

--- src/app/(dashboard)/dashboard/client/_hook/useClientQuery.tsx
+++ src/app/(dashboard)/dashboard/client/_hook/useClientQuery.tsx
@@ -13,13 +13,16 @@ type TClientQueryResult = {
   totalClients: number;
 };
 
+
+export const CLIENT_LIST_KEY = "clients";
+
 export default function useClientQuery({
   search,
   currentPage = 1,
   pageSize = 50,
 }: TClientQueryParam) {
   return useQuery({
-    queryKey: ["clients", search, currentPage, pageSize],
+    queryKey: [CLIENT_LIST_KEY, search, currentPage, pageSize],
     queryFn: async () => {
       const { clients, totalClients } = await getClientList(
         {
