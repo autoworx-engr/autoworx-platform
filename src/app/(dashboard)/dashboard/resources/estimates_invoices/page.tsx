8ea533d6dce7333ec408cf6732f1b48c5cfcfdc0
Mh-Monzil <monzil246@gmail.com>
2025-11-16 18:40:40 +0600

    Refactor resource pages and components to enhance layout consistency and improve UI responsiveness

--- src/app/(dashboard)/dashboard/resources/estimates_invoices/page.tsx
+++ src/app/(dashboard)/dashboard/resources/estimates_invoices/page.tsx
@@ -62,7 +62,7 @@ const page = () => {
         description="Explore the various resources available for estimates and invoices."
         setFilter={setFilter}
       />
-      <div className="mt-4">
+      <div className="mt-4 space-y-5">
         {resourcesData
           .filter((resource) =>
             resource.title.toLowerCase().includes(filter.search.toLowerCase())
