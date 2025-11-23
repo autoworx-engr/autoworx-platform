b6118fc697150192e51abc3e9aea41bd6d515d5c
AbuBokorprog <abubokor1066@gmail.com>
2025-11-23 14:46:12 +0600

    WB-382

--- src/app/(dashboard)/dashboard/payments/components/PaymentTable.tsx
+++ src/app/(dashboard)/dashboard/payments/components/PaymentTable.tsx
@@ -35,7 +35,8 @@ export default function PaymentTable({
       paymentMethod === "Other" &&
       method !== "Card" &&
       method !== "Cash" &&
-      method !== "Cheque"
+      method !== "Cheque" &&
+      method !== "Deposit"
     ) {
       return true;
     } else {
