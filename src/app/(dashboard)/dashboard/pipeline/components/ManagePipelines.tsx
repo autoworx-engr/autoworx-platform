8565d243759b0ca9e141d35a691e6624d2873e54
anasahammad <anasahammad2002@gmail.com>
2025-11-29 22:20:18 +0600

    refactor: streamline column management logic in ManagePipelines component

--- src/app/(dashboard)/dashboard/pipeline/components/ManagePipelines.tsx
+++ src/app/(dashboard)/dashboard/pipeline/components/ManagePipelines.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useDrag, useDrop, DndProvider } from "react-dnd";
+import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import {
