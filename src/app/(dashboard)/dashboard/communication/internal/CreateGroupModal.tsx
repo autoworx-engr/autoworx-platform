ba7df42a5af12fc3de0880247683a779d0f13c2b
anasahammad <anasahammad2002@gmail.com>
2025-12-02 20:28:10 +0600

    fix: adjust modal width and improve user details layout in CreateGroupModal component

--- src/app/(dashboard)/dashboard/communication/internal/CreateGroupModal.tsx
+++ src/app/(dashboard)/dashboard/communication/internal/CreateGroupModal.tsx
@@ -200,7 +200,7 @@ export default function CreateGroupModal({
         </button>
       </DialogTrigger>
       {/* Dialog Content: The main modal body with glassmorphism effect */}
-      <DialogContent className="max-w-[350px] rounded-2xl bg-background  sm:w-full sm:max-w-lg">
+      <DialogContent className="w-[90vw] max-w-md rounded-2xl bg-background  ">
         {error && (
           <p className="text-center text-sm text-rose-500 dark:text-rose-400">
             {error}
@@ -269,7 +269,7 @@ export default function CreateGroupModal({
                             {user.firstName} {user.lastName}
                           </p>
                           {/* Details with muted text color */}
-                          <div className="flex items-center space-x-3 text-xs text-slate-600 dark:text-slate-400">
+                          <div className="flex flex-col md:flex-row md:items-center md:space-x-3 text-[8px] md:text-xs text-slate-600 dark:text-slate-400">
                             {user.phone && <p>{user.phone}</p>}
                             <p className="truncate">{user.email}</p>
                           </div>
