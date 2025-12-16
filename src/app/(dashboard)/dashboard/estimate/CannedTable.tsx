243895a7e7996c0857f36842adfb9de26870cf4b
Ashik-Rahman100 <ashikrahman5515@gmail.com>
2025-12-16 15:13:55 +0600

    WB-628, WB-653, WB-617

--- src/app/(dashboard)/dashboard/estimate/CannedTable.tsx
+++ src/app/(dashboard)/dashboard/estimate/CannedTable.tsx
@@ -9,15 +9,15 @@ type Props = {
 
 const CannedTable = (props: Props) => {
   return (
-   <div className="flex min-h-[65vh] w-full flex-col py-2 lg:flex-row lg:gap-x-4">
+    <div className="flex min-h-[65vh] w-full flex-col py-2 lg:flex-row lg:gap-x-4">
       {/* Canned Labor Wrapper */}
       {/* 2. Ensure inner wrappers use h-full to inherit the height and grow with flex-1 */}
-      <div className="w-full lg:basis-1/2 flex flex-col flex-1 h-full"> 
+      <div className="w-full lg:basis-1/2 flex flex-col flex-1 h-full">
         <CannedLabor labors={props.labors} />
       </div>
 
       {/* Canned Services Wrapper */}
-      <div className="w-full lg:basis-1/2 flex flex-col flex-1 h-full mt-4 lg:mt-0"> 
+      <div className="w-full lg:basis-1/2 flex flex-col flex-1 h-full mt-4 lg:mt-0">
         <CannedServices services={props.services} />
       </div>
     </div>
