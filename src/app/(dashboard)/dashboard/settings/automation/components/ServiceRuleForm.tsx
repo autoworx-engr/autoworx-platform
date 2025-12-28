6129cbe40ea84775b589233ef3f449fb9dd6bc9e
anasahammad <anasahammad2002@gmail.com>
2025-12-28 15:55:11 +0600

    Update template variable descriptions for clarity and add new allowed company ID

--- src/app/(dashboard)/dashboard/settings/automation/components/ServiceRuleForm.tsx
+++ src/app/(dashboard)/dashboard/settings/automation/components/ServiceRuleForm.tsx
@@ -65,13 +65,13 @@ export type Rule = {
 
 // Template variables
 const template_variable_options = [
-  { name: "<CONTACT>", description: "Contact" },
+  { name: "<CLIENT>", description: "Client name" },
   { name: "<INTEREST>", description: "Interest" },
-  { name: "<VEHICLE>", description: "Vehicle" },
+  { name: "<VEHICLE>", description: "Vehicle details" },
   { name: "<SERVICE>", description: "Service" },
   { name: "<BUSINESS_NAME>", description: "Your business name" },
-  { name: "<BUSINESS_PHONE>", description: "Your business phone" },
-  { name: "<BUSINESS_ADDRESS>", description: "Your business address" },
+  { name: "<PHONE>", description: "Your business phone number" },
+  { name: "<ADDRESS>", description: "Your business address" },
   { name: "<VIDEO_DIRECTION>", description: "Video direction" },
   { name: "<GOOGLE_MAP_LINK>", description: "Google map link" },
 ];
