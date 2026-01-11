b2b32a2dba6c98286940545825b90ca5127b13d8
Ashik-Rahman100 <ashikrahman5515@gmail.com>
2026-01-11 23:56:55 +0600

    feat: add user role checks for admin and manager in calendar settings form

--- src/app/(dashboard)/dashboard/task/_component/calendar/General.tsx
+++ src/app/(dashboard)/dashboard/task/_component/calendar/General.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CalendarSettings } from "@prisma/client";
+import { CalendarSettings, EmployeeType } from "@prisma/client";
 
 import { DialogClose, DialogFooter } from "@/components/Dialog";
 import Submit from "@/components/Submit";
@@ -26,7 +26,14 @@ type TGeneralProps = {
   authUser?: any;
 };
 
-export default function General({ settings, onClose }: TGeneralProps) {
+export default function General({
+  settings,
+  onClose,
+  authUser,
+}: TGeneralProps) {
+  const isAdmin = authUser?.user.employeeType === EmployeeType?.Admin;
+  const isManager = authUser?.user.employeeType === EmployeeType?.Manager;
+
   const [error, setError] = useState<string | null>(null);
   const queryClient = useQueryClient();
   async function handleSave(data: FormData) {
@@ -76,117 +83,126 @@ export default function General({ settings, onClose }: TGeneralProps) {
         </div>
       )}
 
-      <form className="flex flex-col gap-5">
-        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
-          <div>
-            <label htmlFor="week-start" className="font-medium">
-              Week Starts
-            </label>
-            <select
-              id="week-start"
-              name="week-start"
-              className="w-full rounded-md border-2 border-gray-400 bg-background p-1 px-2 sm:w-32"
-              defaultValue={settings && settings.weekStart}
-            >
-              <option value="Sunday">Sunday</option>
-              <option value="Monday">Monday</option>
-              <option value="Tuesday">Tuesday</option>
-              <option value="Wednesday">Wednesday</option>
-              <option value="Thursday">Thursday</option>
-              <option value="Friday">Friday</option>
-              <option value="Saturday">Saturday</option>
-            </select>
+      {isAdmin || isManager ? (
+        <form className="flex flex-col gap-5">
+          <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+            <div>
+              <label htmlFor="week-start" className="font-medium">
+                Week Starts
+              </label>
+              <select
+                id="week-start"
+                name="week-start"
+                className="w-full rounded-md border-2 border-gray-400 bg-background p-1 px-2 sm:w-32"
+                defaultValue={settings && settings.weekStart}
+              >
+                <option value="Sunday">Sunday</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="day-start" className="font-medium">
+                Day starts
+              </label>
+              <input
+                type="time"
+                id="day-start"
+                name="day-start"
+                defaultValue={settings ? settings.dayStart : "10:00"}
+                className="w-full rounded-md border-2 border-slate-400 p-1 sm:w-auto"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="day-end" className="font-medium">
+                Day ends
+              </label>
+              <input
+                type="time"
+                id="day-end"
+                name="day-end"
+                defaultValue={settings ? settings.dayEnd : "18:00"}
+                className="w-full rounded-md border-2 border-slate-400 p-1 sm:w-auto"
+              />
+            </div>
           </div>
 
           <div>
-            <label htmlFor="day-start" className="font-medium">
-              Day starts
-            </label>
-            <input
-              type="time"
-              id="day-start"
-              name="day-start"
-              defaultValue={settings ? settings.dayStart : "10:00"}
-              className="w-full rounded-md border-2 border-slate-400 p-1 sm:w-auto"
-            />
-          </div>
+            <div>
+              <p className="font-medium">Show Weekends</p>
+              <div className="flex gap-5">
+                <div className="w-full">
+                  <select
+                    id="weekend-1"
+                    name="weekend-1"
+                    className="w-full rounded-md border border-[#6571FF] bg-[#DDE0FF] p-1 text-center text-[#6571FF] focus:border-[#6571FF] focus:bg-background focus:text-black"
+                    defaultValue={settings && settings.weekend1}
+                  >
+                    {week.map((day) => (
+                      <option key={day} value={day}>
+                        {day}
+                      </option>
+                    ))}
+                  </select>
+                </div>
 
-          <div>
-            <label htmlFor="day-end" className="font-medium">
-              Day ends
-            </label>
-            <input
-              type="time"
-              id="day-end"
-              name="day-end"
-              defaultValue={settings ? settings.dayEnd : "18:00"}
-              className="w-full rounded-md border-2 border-slate-400 p-1 sm:w-auto"
-            />
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <p className="font-medium">Show Weekends</p>
-            <div className="flex gap-5">
-              <div className="w-full">
-                <select
-                  id="weekend-1"
-                  name="weekend-1"
-                  className="w-full rounded-md border border-[#6571FF] bg-[#DDE0FF] p-1 text-center text-[#6571FF] focus:border-[#6571FF] focus:bg-background focus:text-black"
-                  defaultValue={settings && settings.weekend1}
-                >
-                  {week.map((day) => (
-                    <option key={day} value={day}>
-                      {day}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              <div className="w-full">
-                <select
-                  id="weekend-2"
-                  name="weekend-2"
-                  className="w-full rounded-md border border-[#6571FF] bg-[#DDE0FF] p-1 text-center text-[#6571FF] focus:border-[#6571FF] focus:bg-background focus:text-black"
-                  defaultValue={settings && settings.weekend2}
-                >
-                  {week.map((day) => (
-                    <option key={day} value={day}>
-                      {day}
-                    </option>
-                  ))}
-                </select>
+                <div className="w-full">
+                  <select
+                    id="weekend-2"
+                    name="weekend-2"
+                    className="w-full rounded-md border border-[#6571FF] bg-[#DDE0FF] p-1 text-center text-[#6571FF] focus:border-[#6571FF] focus:bg-background focus:text-black"
+                    defaultValue={settings && settings.weekend2}
+                  >
+                    {week.map((day) => (
+                      <option key={day} value={day}>
+                        {day}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
+          <div className="h-36 rounded-md bg-[#FAFAFA] p-3">
+            <div className="flex flex-wrap justify-between gap-2">
+              <p className="font-semibold md:mt-[6px]">Google Calendar Api</p>
+              <ConnectGoogle />
+            </div>
+          </div>
+
+          {/* Footer */}
+          <DialogFooter>
+            <DialogClose asChild>
+              <button
+                type="button"
+                className="rounded-md border-2 border-slate-400 p-1"
+              >
+                Cancel
+              </button>
+            </DialogClose>
+            <Submit
+              className="mb-2 rounded-md bg-[#6571FF] p-1 px-5 text-white md:mb-0"
+              formAction={handleSave}
+            >
+              Save
+            </Submit>
+          </DialogFooter>
+        </form>
+      ) : (
         <div className="h-36 rounded-md bg-[#FAFAFA] p-3">
           <div className="flex flex-wrap justify-between gap-2">
             <p className="font-semibold md:mt-[6px]">Google Calendar Api</p>
             <ConnectGoogle />
           </div>
         </div>
-
-        {/* Footer */}
-        <DialogFooter>
-          <DialogClose asChild>
-            <button
-              type="button"
-              className="rounded-md border-2 border-slate-400 p-1"
-            >
-              Cancel
-            </button>
-          </DialogClose>
-          <Submit
-            className="mb-2 rounded-md bg-[#6571FF] p-1 px-5 text-white md:mb-0"
-            formAction={handleSave}
-          >
-            Save
-          </Submit>
-        </DialogFooter>
-      </form>
+      )}
     </>
   );
 }
