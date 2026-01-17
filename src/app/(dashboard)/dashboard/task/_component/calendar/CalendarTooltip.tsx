cc085f0343c59b1ba943513b8b498dc45c2197e5
anasahammad <anasahammad2002@gmail.com>
2026-01-17 20:07:40 +0600

    feat: add close functionality to TaskTooltip and integrate CalendarTooltip in DayTask

--- src/app/(dashboard)/dashboard/task/_component/calendar/CalendarTooltip.tsx
+++ src/app/(dashboard)/dashboard/task/_component/calendar/CalendarTooltip.tsx
@@ -133,6 +133,7 @@ export default function CalendarTooltip({
               onModalOpen={() => {
                 setIsTaskModalOpen(true);
               }}
+              onClose={onClose}
             />
           )}
         </TooltipContent>
