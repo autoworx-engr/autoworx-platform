50f6bfee972f89f23ad79157f9f564f81c3a482f
Naimur00 <aminaim4u@gmail.com>
2026-01-26 18:14:39 +0600

    fix: responsive issue in admin dashboard

--- src/app/(dashboard)/dashboard/components/DashboardAdmin.tsx
+++ src/app/(dashboard)/dashboard/components/DashboardAdmin.tsx
@@ -9,30 +9,34 @@ import TaskListBox from "./box/TaskListBox";
 
 const Dashboard = async () => {
   return (
-    <div className="min-h-screen w-full bg-[#f8fafc] p-4 sm:p-0 dark:bg-[#0f172a]">
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-4 items-stretch">
+    <div className="h-screen w-full bg-[#f8fafc] p-4 dark:bg-[#0f172a]">
+      <div className="grid h-full grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-4 items-stretch">
         
-        <div className="flex flex-col gap-4 xl:gap-6">
+        {/* Column 1 */}
+        <div className="flex flex-col gap-2 xl:gap-4">
           <SalesPipelineBox />
           <ShopPipelineBox />
         </div>
 
-        <div className="flex flex-col gap-4 xl:gap-6">
+        {/* Column 2 */}
+        <div className="flex flex-col gap-2 xl:gap-4">
           <RevenueBox />
           <InventoryBox />
           <EmployeePayoutBox />
         </div>
 
-        <div className="flex flex-col">
+        {/* Column 3 */}
+        <div className="flex flex-col h-full">
           <AppointmentListBox />
         </div>
 
+        {/* Column 4 */}
         <div className="md:col-span-full lg:col-span-1">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 lg:h-full xl:gap-6">
-            <div className="flex flex-col h-full">
+          <div className="flex flex-col md:flex-row lg:flex-col h-full gap-2 xl:gap-4">
+            <div className="flex-1 min-h-0 h-[25rem]">
               <TaskListBox />
             </div>
-            <div className="flex flex-col h-full">
+            <div className="flex-1 min-h-0 h-[25rem]">
               <ReputationBox />
             </div>
           </div>
