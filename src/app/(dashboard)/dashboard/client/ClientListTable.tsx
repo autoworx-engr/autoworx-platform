ec8d1b7493f7a934aaa8668b63891ea1ff5de0d7
Ashik-Rahman100 <ashikrahman5515@gmail.com>
2026-01-28 14:55:08 +0600

    refactor: improve layout and styling of client, employee, and fleet tables

--- src/app/(dashboard)/dashboard/client/ClientListTable.tsx
+++ src/app/(dashboard)/dashboard/client/ClientListTable.tsx
@@ -33,9 +33,9 @@ const ClientListTable = ({
   const showPagination = totalClients > pageSize;
 
   return (
-    <div className="hidden lg:block overflow-hidden rounded-xl p-2 bg-white dark:bg-slate-900 ring-1 ring-slate-200 dark:ring-slate-800 shadow-sm">
-      <div>
-        <table className="w-full">
+    <div className="hidden lg:block rounded-xl bg-white dark:bg-slate-900 ring-1 ring-slate-200 dark:ring-slate-800 shadow-sm overflow-hidden p-2">
+      <div className="max-h-[70vh] overflow-y-auto overflow-x-auto">
+        <table className="w-full border-separate border-spacing-0">
           <thead className="sticky top-0 bg-background">
             <tr className="h-10 border-b">
               <th className="border-b px-4 py-2 text-left">Client ID</th>
