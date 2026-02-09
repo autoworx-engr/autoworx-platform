25e5903360bf950dda435199626076a0d0df47ef
mhlehri <lehrimirza101@gmail.com>
2026-02-09 11:35:11 +0600

    feat: Implement sales pipeline dashboard with lead search, filtering, and infinite scrolling capabilities.

---
+++ src/app/(dashboard)/dashboard/pipeline/sales/pipeline/_components/FilterLead.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Filter } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import ResetButton from "./ResetButton";
+
+export default function FilterLead({ searchParams }: { searchParams: any }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentOrder = searchParams.orderBy || undefined;
+
+  const onValueChange = (value: string) => {
+    const params = new URLSearchParams();
+    if (searchParams.searchTerm) {
+      params.set("searchTerm", searchParams.searchTerm);
+    }
+    params.set("orderBy", value);
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Select onValueChange={onValueChange} value={currentOrder || ""}>
+        <SelectTrigger showIcon={false} className="bg-white h-11 hover:border-slate-200 hover:bg-slate-50/30 rounded-xl border-2 border-slate-100 justify-start outline-none focus:border-[#6571FF]/40 focus:bg-white focus:ring-4 focus:ring-[#6571FF]/10">
+          {/* <SelectValue className="flex gap-1 flex-row"> */}
+          <Filter />{" "}
+          {currentOrder === "asc"
+            ? "Oldest"
+            : currentOrder === "desc"
+              ? "Newest"
+              : "Sort by"}
+        </SelectTrigger>
+        <SelectContent position="popper">
+          <SelectGroup>
+            <SelectItem value="desc">Newest</SelectItem>
+            <SelectItem value="asc">Oldest</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      {(searchParams.searchTerm || searchParams.orderBy) && <ResetButton />}
+    </div>
+  );
+}
--- src/app/(dashboard)/dashboard/pipeline/sales/pipeline/_components/ResetButton.tsx
+++ src/app/(dashboard)/dashboard/pipeline/sales/pipeline/_components/ResetButton.tsx
@@ -1,20 +1,17 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
-export default function ResetButton({ searchParams }: { searchParams: { searchTerm?: string; orderBy?: string } }) {
+export default function ResetButton() {
     const router = useRouter();
     const pathname = usePathname();
     const handleResetFilters = () => {
         router.replace(`${pathname}`);
-        // router.refresh();
     };
 
     return (
-        <Button variant="destructive" className="h-11 rounded-xl" onClick={handleResetFilters}>
-            <X size={18} />
-            Reset Filters
+        <Button className="h-10 rounded-xl hover:bg-red-700 bg-red-600" onClick={handleResetFilters}>
+            Clear
         </Button>
     );
-}
+}   
