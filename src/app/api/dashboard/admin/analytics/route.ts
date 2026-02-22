ff430e24c98b05a42308084a23981c94aae78a6e
AbuBokorprog <abubokor1066@gmail.com>
2026-02-22 12:41:34 +0600

    Refactored dashboard analytics api

--- src/app/api/dashboard/admin/analytics/route.ts
+++ src/app/api/dashboard/admin/analytics/route.ts
@@ -235,9 +235,19 @@ export async function GET(req: NextRequest) {
       company?.timezone || Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     //sales pipeline
-    const totalLeadsPerMonthPromise = getTotalLeadsPerMonth(timezone);
-    const leadsConvertedDataPromise = getConvertedLeadsPerMonth(timezone);
-    const conversionRateDataPromise = getConversionRateWithGrowth(timezone);
+
+    const totalLeadsPerMonthPromise = getTotalLeadsPerMonth(
+      timezone,
+      companyId,
+    );
+    const leadsConvertedDataPromise = getConvertedLeadsPerMonth(
+      timezone,
+      companyId,
+    );
+    const conversionRateDataPromise = getConversionRateWithGrowth(
+      timezone,
+      companyId,
+    );
 
     const [totalLeadsPerMonth, leadsConvertedData, conversionRateData] =
       await Promise.all([
@@ -252,9 +262,9 @@ export async function GET(req: NextRequest) {
     const conversionRateGrowth = conversionRateData.conversionRateGrowth;
 
     //shop pipeline
-    const completedJobsPromise = getCompletedJobs(timezone);
-    const totalJobsPromise = getTotalJobs();
-    const ongoingJobsPromise = getOngoingJobs();
+    const completedJobsPromise = getCompletedJobs(timezone, companyId);
+    const totalJobsPromise = getTotalJobs(companyId);
+    const ongoingJobsPromise = getOngoingJobs(companyId);
 
     const [completedJobsData, totalJobsData, ongoingJobsData] =
       await Promise.all([
@@ -274,11 +284,11 @@ export async function GET(req: NextRequest) {
       completedJobsData?.growth?.isPositive ?? false;
 
     //revenue
-    const revenue = await getRevenue(timezone);
-    const expectedRevenue = await getExpectedRevenue();
+    const revenue = await getRevenue(timezone, companyId);
+    const expectedRevenue = await getExpectedRevenue(companyId);
 
     //inventory
-    const inventory = await getInventory(timezone);
+    const inventory = await getInventory(timezone, companyId);
 
     // Clean data extraction and parsing
     const totalValue = inventory?.totalValue || 0;
@@ -289,7 +299,7 @@ export async function GET(req: NextRequest) {
     const isInventoryPositive = inventory?.growth?.isPositive ?? false;
 
     //employee box
-    const employeePayout = await getEmployeePayout(timezone);
+    const employeePayout = await getEmployeePayout(timezone, companyId);
 
     // Data Extraction and Formatting
     const currentMonthTotalPayout = employeePayout?.currentMonthTotal || 0;
@@ -360,6 +370,7 @@ export async function GET(req: NextRequest) {
           date: "asc",
         },
         ...fetchWithAppointment,
+        take: 10,
       });
     } else {
       // Logic for Technician or Other role
@@ -386,6 +397,7 @@ export async function GET(req: NextRequest) {
           date: "asc",
         },
         ...fetchWithAppointment,
+        take: 10,
       });
     }
 
@@ -408,6 +420,7 @@ export async function GET(req: NextRequest) {
 
     tasks = await db.task.findMany({
       where: whereCondition,
+      take: 20,
     });
 
     totalTasks = await db.task.count({
@@ -418,6 +431,7 @@ export async function GET(req: NextRequest) {
     });
 
     const data = {
+      user,
       currentTotalLeads,
       currentConversionRate,
       conversionRateGrowth,
