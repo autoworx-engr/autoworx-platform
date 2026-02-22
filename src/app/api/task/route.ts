21f9d7da84b52d7d14599f50415fe6336b5d125d
AbuBokorprog <abubokor1066@gmail.com>
2026-02-23 04:32:13 +0600

    task api created

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -148,18 +148,6 @@ import createGoogleCalendarEvent from "@/actions/task/google-calendar/createGoog
  */
 export async function POST(req: NextRequest) {
   try {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Unauthorized",
-        },
-        { status: 401 },
-      );
-    }
-
     const body = await req.json();
 
     const {
