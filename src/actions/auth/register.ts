76a53398391bb7a1fdb3cfd7169f7e387be19719
Mh-Monzil <monzil246@gmail.com>
2026-03-09 23:14:52 +0600

    fix: update invoice message in register function and correct default avatar path in ResponsiveEmployeeCard component

--- src/actions/auth/register.ts
+++ src/actions/auth/register.ts
@@ -289,7 +289,7 @@ export async function register({
     await db.companyEmailTemplate.create({
       data: {
         subject: `Estimate for services requested at <BUSINESS_NAME>`,
-        message: `Hey <CLIENT>, your invoice for <VEHICLE> is ready. If everything looks good, please approve it so we can move forward. Thanks!– <BUSINESS_NAME>`,
+        message: `Hey <CLIENT>, your estimate for <VEHICLE> is ready. If everything looks good, please approve it so we can move forward. Thanks!– <BUSINESS_NAME>`,
         companyId: newCompany.id,
       },
     });
