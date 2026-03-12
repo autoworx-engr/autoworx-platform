890384274ea8ce5a912c7d0e53274d26147297b2
valdimir-makarov <2010049@student.ruet.ac.bd>
2026-03-12 23:39:53 +0600

    fixed the Requried Sections only

--- src/app/(dashboard)/dashboard/client/EditClientModalBody.tsx
+++ src/app/(dashboard)/dashboard/client/EditClientModalBody.tsx
@@ -207,7 +207,10 @@ export default function EditClientModalBody({
   }, []);
 
   return (
-    <DialogContent className="max-h-full max-w-2xl grid-rows-[auto,1fr,auto]">
+    <DialogContent
+      className="max-h-full max-w-2xl grid-rows-[auto,1fr,auto]"
+      onOpenAutoFocus={(e) => e.preventDefault()}
+    >
       <div className="mt-8 flex items-center justify-between px-2">
         <div>
           <h1 className="text-2xl font-bold tracking-tight text-slate-600 dark:text-slate-100">
