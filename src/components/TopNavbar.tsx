d346ddf0304d6bb479230d9891a0d7807a1b3cb3
AbuBokorprog <abubokor1066@gmail.com>
2026-04-19 12:14:55 +0600

    virtual shop feature permission enabled

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,10 +1,16 @@
+import { useCompanyFeaturePermissionStore } from "@/stores/companyFeaturePermissionStore";
 import TopNavbarIcons from "./TopNavbarIcons";
 import ShopList from "./top-navbar/ShopList";
 
 export default function TopNavbar() {
+  const { companyFeaturePermission } = useCompanyFeaturePermissionStore();
+  const virtualShopPermission = companyFeaturePermission.find(
+    (p) => p.permission_name === "virtual-shop",
+  );
+
   return (
     <div className="hidden h-[6vh] items-center justify-end p-5 pr-10 sm:flex">
-      <ShopList />
+      {virtualShopPermission?.enabled && <ShopList />}
       <TopNavbarIcons />
     </div>
   );
