fcf34e69baee798fee072849464d5fb9caae61ce
AbuBokorprog <abubokor1066@gmail.com>
2026-05-05 18:45:34 +0600

    add logs in sms receive webhooks

--- src/service/ai-agent/api.ts
+++ src/service/ai-agent/api.ts
@@ -12,8 +12,9 @@ type TSendSMSToAIAgent = {
 
 export const sendSMSToAgent = async function (payload: TSendSMSToAIAgent) {
   try {
+    console.log("Send to sales for reply", payload);
     const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_SALES_AGENT_API_BASE_URL}/webhook/a7db912a-d7ef-42c7-bc2f-89cb49ac39b9`,
+      `${process.env.NEXT_PUBLIC_SALES_AGENT_API_BASE_URL}`,
       payload,
     );
 
