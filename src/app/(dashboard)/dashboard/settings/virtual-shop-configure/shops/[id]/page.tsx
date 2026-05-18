6e64d03f554a2b1044f7eee072703b54a4b0e11c
AbuBokorprog <abubokor1066@gmail.com>
2026-05-18 10:08:47 +0600

    feat:In virtual shop, After clicking on configure new shop redirect to create new but there is no back button to go back

--- src/app/(dashboard)/dashboard/settings/virtual-shop-configure/shops/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/settings/virtual-shop-configure/shops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ShopForm from "../../component/ShopForm";
 import { getCompanyId } from "@/lib/companyId";
 
@@ -7,6 +8,11 @@ export default async function UpdateShopPage({ params }: { params: any }) {
 
   return (
     <div className="p-6">
+      <Link href={"/dashboard/settings/virtual-shop-configure"}>
+        <button className="flex w-20 items-center justify-center gap-2 rounded-xl bg-[#6571FF] py-3 font-medium text-white shadow-sm transition-colors hover:bg-[#6571FF]/90 disabled:cursor-not-allowed disabled:opacity-70">
+          Back
+        </button>
+      </Link>
       <ShopForm shopId={Number(id)} companyId={companyId} />
     </div>
   );
