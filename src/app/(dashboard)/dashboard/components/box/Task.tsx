c75beeadc47fbbd5ab1db1cd6dd009824ce46117
mhlehri <lehrimirza101@gmail.com>
2026-06-09 16:11:35 +0600

    fix: date time issue in tasklist in dashboard.

--- src/app/(dashboard)/dashboard/components/box/Task.tsx
+++ src/app/(dashboard)/dashboard/components/box/Task.tsx
@@ -34,7 +34,7 @@ const Task = ({ task, onTaskDeleted }: TaskProps) => {
   const getTaskDateTimeSummary = (task: TaskType) => {
     if (!task.date) return null;
 
-    const taskMoment = moment.utc(task.date).tz(timezone);
+    const taskMoment = moment.utc(task.date);
     const timeFormat = task.startTime
       ? moment(task.startTime, "HH:mm").format("h:mmA")
       : null;
@@ -54,9 +54,7 @@ const Task = ({ task, onTaskDeleted }: TaskProps) => {
     : null;
 
   // Get date part
-  const datePart = task.date
-    ? moment.utc(task.date).tz(timezone).format("MMM DD")
-    : null;
+  const datePart = task.date ? moment.utc(task.date).format("MMM DD") : null;
 
   const handleTaskClick = () => {
     const dateString = task?.date
