ac56ad0c9c083407e1a54d96766bfa66dc69646b
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2026-08-06 21:12:18 +0600

    updated the client msg check to also include employee type check

--- src/app/(dashboard)/dashboard/components/box/MessageContainer.tsx
+++ src/app/(dashboard)/dashboard/components/box/MessageContainer.tsx
@@ -206,7 +206,12 @@ export default function MessageContainer({
               endMessage={
                 hasMessages && !hasMore ? (
                   <p className="text-center text-sm text-slate-400 dark:text-slate-500 py-4">
-                    <b>All messages loaded</b>
+                    {user?.employeeType === "Sales" &&
+                    clientMessages.length === 0 ? (
+                      <b>Client Not Found</b>
+                    ) : (
+                      <b>All messages loaded</b>
+                    )}
                   </p>
                 ) : null
               }
