a2f5af27334e601289a72493c41cafb828d461c8
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2026-08-09 20:00:51 +0600

    updated the grid breaking point to fix the ui breaking in financials card

--- src/app/(dashboard)/dashboard/settings/estimates/EstimateAndInvoicePage.tsx
+++ src/app/(dashboard)/dashboard/settings/estimates/EstimateAndInvoicePage.tsx
@@ -122,7 +122,7 @@ export default function EstimateAndInvoicePage() {
           <div className="space-y-4">
             {isLoading ? (
               <div className="space-y-6">
-                <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-1 2xl:grid-cols-2">
                   {Array.from({ length: 3 }).map((_, index) => (
                     <div
                       key={`financial-skeleton-${index + 1}`}
@@ -139,7 +139,7 @@ export default function EstimateAndInvoicePage() {
               </div>
             ) : (
               <>
-                <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-1 2xl:grid-cols-2">
                   <div className="relative">
                     <SlimInput
                       name="taxAmount"
@@ -174,7 +174,7 @@ export default function EstimateAndInvoicePage() {
                     />
                   </div>
 
-                  <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 sm:col-span-2 md:col-span-1 2xl:col-span-2">
                     <label className="px-1 font-medium text-slate-600">
                       Currency
                     </label>
